099d4f4babcc90fdf467203c8da61a8329984395
timothyleconte <marktbooher@outlook.com>
2025-07-01 00:40:32 -0400

    Deleted old, unused and pseudocode

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
-//assign a number to each option (rock, paper, scissors) 
-//have computer output 1 of 3 numbers at random
-//display the chosen variable
+//mark's first ever javascript game!
 
 function getComputerChoice(){
 
@@ -15,31 +13,8 @@ function getComputerChoice(){
     }
 }
 
-//prompt the player to enter their choice (rock, paper, scissors)
-//take their choice and turn it into a case-insensitive variable
-
-// function getHumanChoice(){
-    
-//     let a = prompt("rock, paper, or scissors?").toLowerCase();
-
-//     if (a == "rock"){
-//         return("rock");
-//     }else if (a == "paper"){
-//         return("paper");
-//     }else {
-//         return("scissors");
-//     }
-// }
-
-//create score variables
-
 let humanScore = 0;
 let computerScore = 0;
-
-//create logic to play a single round of rock paper scissors
-//take the two players choices as arguments and output the winner using rock, paper, scissors rules
-//take the winner of the playRound and increment their score by 1; display scoreboard
-
 
 
 function playRound(humanChoice, computerChoice){
@@ -79,44 +54,17 @@ function playRound(humanChoice, computerChoice){
     humScore.textContent = `Your score: ${humanScore}`;
     compScore.textContent = `Computer's score: ${computerScore}`;
 
-    if (humanScore === 5 && computerScore === 5){
-        results.textContent = "You tied the computer! Try again.";
-        humanScore = 0;
-        computerScore = 0;
-    }else if (humanScore === 5 && computerScore < 5){
+    if (humanScore === 5){
         results.textContent = "You win! Congratulations - the computer owes you a beer!";
         humanScore = 0;
         computerScore = 0;
-    }else if (computerScore === 5 && humanScore < 5){
+    }else if (computerScore === 5){
         results.textContent = "You lose! Sorry - buy the computer a beer!"
         humanScore = 0;
         computerScore = 0;
     }
 
 }
-
-
-//start new code for playing an entire game of rock paper scissors which is the best 3 out of 5 (total five rounds)
-//repeat the playRound code five times to play the full game and end with a winning/losing message
-
-// function playGame(){
-//     playRound(getHumanChoice(),getComputerChoice());
-//        playRound(getHumanChoice(),getComputerChoice());
-//           playRound(getHumanChoice(),getComputerChoice());
-//              playRound(getHumanChoice(),getComputerChoice());
-//                 playRound(getHumanChoice(),getComputerChoice());
-
-//     if (humanScore > computerScore){
-//         console.log("You win! Congratulations - free beers on the computer! Refresh to play again!");
-//     }else if (computerScore > humanScore){
-//         console.log("You lose! Sorry - buy the computer a beer! Refresh to play again!");
-//     }else{
-//         console.log("You tied the computer -- refresh to try again!")
-//     }
-
-// }
-
-
 
 const rockButton = document.querySelector("#rock");
 
